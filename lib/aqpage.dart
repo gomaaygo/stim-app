ebe404f854ee7d0849625767736032bde8898cf6
Maiury Garcia <maiurygarcia@gmail.com>
2024-03-17 11:52:40 -0300

    feat: Dynamic quiz

--- lib/aqpage.dart
+++ lib/aqpage.dart
@@ -1,12 +1,60 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sort_child_properties_last
 
+import 'package:appstim/homepage.dart';
 import 'package:flutter/material.dart';
 import 'form-personal-data.dart';
 import 'formfields.dart';
 import 'appbar.dart';
+import 'dart:convert';
+import 'dart:io';
 
-class AQ10 extends StatelessWidget {
-  const AQ10({super.key});
+class AQ10Screen extends StatefulWidget {
+  @override
+  _AQ10ScreenState createState() => _AQ10ScreenState();
+}
+
+class _AQ10ScreenState extends State<AQ10Screen> {
+  int currentQuestion = 0;
+  List<String> questions = [
+    'A sua criança olha	para si	quando chama pelo	nome dela?',
+    'Quão fácil	é	para si conseguir	contato	ocular com a sua criança?',
+    'A sua criança aponta	para indicar que quer alguma coisa? (ex: um	brinquedo	que	está fora	do alcance)',
+    'A sua criança aponta	para partilhar um	interesse	consigo? (ex: apontar	para um	cenário interessante)',
+    'A sua criança “faz	de conta”? (ex: ao cuidar	de bonecas, falar	num	telefone de	brincar)',
+    'A	sua	criança	segue	o	seu olhar?',
+    'Se	você ou	alguém da	sua	família	estiver visivelmente aborrecido, a sua criança mostra	sinais de	querer confortá-lo?	(ex: acariciando o cabelo, abraçando)',
+    'Descreveria as	primeiras	palavras da	sua	criança	como:',
+    'A sua criança usa gestos simples? (ex: acenar adeus)',
+    'A sua criança olha	fixamente	para nada sem	razão aparente?'
+  ];
+  List<int> feedbackList = List.filled(10, 0);
+
+  void submitFeedback(int rating) {
+    setState(() {
+      feedbackList[currentQuestion] = rating;
+      if (currentQuestion < 9) {
+        currentQuestion++;
+      } else {
+        print("Feedback submitted: $feedbackList");
+
+        // Após responder à última pergunta, navega para a página inicial.
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      }
+    });
+  }
+
+  Future<void> saveFeedbackToJson() async {
+    Map<String, dynamic> feedbackData = {
+      'questions': questions,
+      'feedback': feedbackList,
+    };
+
+    final file = File('feedback_data.json');
+    await file.writeAsString(jsonEncode(feedbackData));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -90,7 +138,30 @@ class AQ10 extends StatelessWidget {
               ],
             ),
           ),
-          FormPersonalData(),
+          Container(
+            child: Form(
+              child: Column(
+                children: [
+                  Padding(
+                    padding:
+                        EdgeInsets.only(top: 20, bottom: 10, left: 3, right: 3),
+                    child: Text(
+                      '${currentQuestion + 1}. ${questions[currentQuestion]}',
+                      style: TextStyle(fontFamily: 'Montserrat', fontSize: 18),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                  RadioOptions(),
+                  Container(
+                    child: Text(
+                      '${currentQuestion + 1}/12',
+                      style: TextStyle(fontFamily: 'Montserrat', fontSize: 15),
+                    ),
+                  ), // Adiciona espaço de 20 unidades entre os widgets
+                ],
+              ),
+            ),
+          ),
           Container(
             child: Row(
               children: [
@@ -99,7 +170,7 @@ class AQ10 extends StatelessWidget {
                       primary: Color.fromRGBO(13, 71, 161, 0.900),
                       minimumSize: Size(70, 65),
                       shape: CircleBorder()),
-                  onPressed: () {},
+                  onPressed: () => submitFeedback(currentQuestion + 1),
                   child: Icon(Icons.navigate_next_outlined,
                       color: Colors.white, size: 40),
                 )
@@ -112,7 +183,6 @@ class AQ10 extends StatelessWidget {
     );
   }
 }
-
 
 class AQPage extends StatelessWidget {
   const AQPage({super.key});
@@ -128,7 +198,7 @@ class AQPage extends StatelessWidget {
         home: Scaffold(
           appBar: AppBarHome(),
           body: SingleChildScrollView(
-            child: AQ10(),
+            child: AQ10Screen(),
           ),
           bottomNavigationBar: BottomNavigationBar(
               currentIndex: 0,
@@ -144,7 +214,6 @@ class AQPage extends StatelessWidget {
                     label: 'Ajuda'),
               ]),
         ),
-        debugShowCheckedModeBanner: false
-        );
+        debugShowCheckedModeBanner: false);
   }
 }
